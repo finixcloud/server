4c23b19be59c7d5296d0b98d7b484d5c67243504
jld3103 <jld3103yt@gmail.com>
2023-07-03 18:59:51 +0200

    Rewrite OCS CSRF check to be readable
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- lib/private/AppFramework/Middleware/Security/SecurityMiddleware.php
+++ lib/private/AppFramework/Middleware/Security/SecurityMiddleware.php
@@ -206,21 +206,9 @@ class SecurityMiddleware extends Middleware {
 		}
 		// CSRF check - also registers the CSRF token since the session may be closed later
 		Util::callRegister();
-		if (!$this->hasAnnotationOrAttribute($reflectionMethod, 'NoCSRFRequired', NoCSRFRequired::class)) {
-			/*
-			 * Only allow the CSRF check to fail on OCS Requests. This kind of
-			 * hacks around that we have no full token auth in place yet and we
-			 * do want to offer CSRF checks for web requests.
-			 *
-			 * Additionally we allow Bearer authenticated requests to pass on OCS routes.
-			 * This allows oauth apps (e.g. moodle) to use the OCS endpoints
-			 */
-			if (!$this->request->passesCSRFCheck() && !(
-				$controller instanceof OCSController && (
-					$this->request->getHeader('OCS-APIREQUEST') === 'true' ||
-					str_starts_with($this->request->getHeader('Authorization'), 'Bearer ')
-				)
-			)) {
+		if (!$this->hasAnnotationOrAttribute($reflectionMethod, 'NoCSRFRequired', NoCSRFRequired::class) && !$this->request->passesCSRFCheck()) {
+			// If CSRF is required and the CSRF check does not pass, only allow OCS requests (OCS-APIREQUEST is true or Authorization is Bearer) to that go to an OCS controllers.
+			if (!$controller instanceof OCSController || ($this->request->getHeader('OCS-APIREQUEST') !== 'true' && !str_starts_with($this->request->getHeader('Authorization'), 'Bearer '))) {
 				throw new CrossSiteRequestForgeryException();
 			}
 		}
